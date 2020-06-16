47d2d10376ad6a651ccda19ea347f0adce1ceced
unknown <2041757928@qq.com>
2020-06-16 23:17:54 +0800

    CC Earth Scence and Koreographer test

---
+++ Assets/KoreDemo/Scripts/BallTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SonicBloom.Koreo;
+
+public class BallTest : MonoBehaviour
+{
+    public string eventID;
+    public Transform ballTransform;
+    public Vector3 scaleChange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballTransform = GetComponentInChildren<Transform>();
+        scaleChange = new Vector3(2f, 2f, 2f);
+        Koreographer.Instance.RegisterForEvents(eventID, BallEvent);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void BallEvent(KoreographyEvent koreographyEvent) {
+        BallUp();
+        //Invoke("BallDown", 0.4f);
+    }
+
+    private void BallUp() {
+        ballTransform.transform.localScale += scaleChange;
+    }
+
+    private void BallDown() {
+        ballTransform.transform.localScale -= scaleChange;
+    }
+
+    
+}
