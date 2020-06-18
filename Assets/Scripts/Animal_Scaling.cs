46cc40aeab610000c56698852e671ab576e99620
JasonZhang-ML <58825716+JasonZhang-ML@users.noreply.github.com>
2020-06-18 21:21:51 +0800

    Fix city_change && Add random test of 5 feature scores

---
+++ Assets/Scripts/Animal_Scaling.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal_Scaling : MonoBehaviour
+{  
+    public static Animal_Scaling _instance;
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public float animal_score; 
+    Vector3 increment;
+    // Start is called before the first frame update
+    void Start()
+    {
+        increment = new Vector3(0.01f, 0.01f, 0.01f);
+    }
+    Vector3 Normalization(Vector3 max, Vector3 min, float score)
+    {
+        float R = score * (max.x - min.x) + min.x;
+        return new Vector3(R,R,R);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 ScaleMax = new Vector3(1.7f,1.7f,1.7f);
+        Vector3 ScaleMin = new Vector3(1f,1f,1f);
+        Vector3 ScaleChange =  Normalization(ScaleMax, ScaleMin, animal_score);
+
+        // Debug.Log(ScaleMax.sqrMagnitude);
+        foreach (Transform child in transform)
+        {
+            if (child.localScale.sqrMagnitude - ScaleChange.sqrMagnitude < 0.05){
+                child.localScale += increment;
+            }
+            else if (child.localScale.sqrMagnitude - ScaleChange.sqrMagnitude >= 0.05) {
+                child.localScale -= increment;
+            }
+        }       
+    }
+}
---
+++ Assets/Scripts/Earth_Color.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Earth_Color : MonoBehaviour
+{
+    // Eanble other scripts call
+    public static Earth_Color _instance;
+    private void Awake()
+    {
+        _instance = this;
+    }
+    public float ground_score;
+    public float water_score;
+
+    // Ground color change from brown to green
+    Color brown = new Color(0.553f, 0.364f, 0.086f, 1f);
+    Color green = new Color(0.086f, 0.553f, 0.114f, 1f);
+
+    // Water color from yellow to blue
+    Color yellow = new Color(0.705f, 1f, 0f, 1f);
+    Color blue = new Color(0f, 0.694f, 1f, 1f);
+    Color Normalization_inverse(Color a, Color b, float score)
+    {
+        float R = score * (b.r - a.r) + a.r;
+        float G = score * (b.g - a.g) + a.g;
+        float B = score * (b.b - a.b) + a.b;
+
+        return new Color(R,G,B,1f);
+    }
+
+    void set_color_score(float ground, float water){
+        ground_score = ground;
+        water_score = water;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Color ground_change = Normalization_inverse(brown, green, ground_score);
+        Color water_change = Normalization_inverse(yellow, blue, water_score);
+
+        Color e = GetComponent<MeshRenderer>().materials[1].color;
+        Color w = GetComponent<MeshRenderer>().materials[0].color;
+
+        GetComponent<MeshRenderer>().materials[1].color = Color.Lerp(e, ground_change, Time.deltaTime * 0.5f);           
+        GetComponent<MeshRenderer>().materials[0].color = Color.Lerp(w, water_change, Time.deltaTime * 0.5f); 
+    }
+
+}
