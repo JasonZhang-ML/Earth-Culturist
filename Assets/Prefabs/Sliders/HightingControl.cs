fab988cb1437b24f597dcb7e474023d043678528
unknown <2041757928@qq.com>
2020-06-20 17:09:16 +0800

    add lighting sliders

---
+++ Assets/Prefabs/Sliders/HightingControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HightingControl : MonoBehaviour
+{
+
+    private HighlightableObject slider; 
+
+    private void Awake() {
+        slider = GetComponent<HighlightableObject>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //slider.ConstantOn(Color.yellow);
+        //slider.ConstantOff();
+        slider.FlashingOn(Color.green, Color.yellow, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
