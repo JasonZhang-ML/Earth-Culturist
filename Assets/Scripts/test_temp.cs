a5b4d00647ece89fbebb7fc9b8c01c5cd2e7bfc6
JasonZhang-ML <58825716+JasonZhang-ML@users.noreply.github.com>
2020-06-20 16:54:38 +0800

    Adjust the movement direction and simplify Sliders.cs

---
+++ Assets/Scripts/test_temp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_temp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Debug.Log(new Vector3(-50.5589f, 42.4239f, 10f).sqrMagnitude);       
+        Debug.Log(new Vector3(0f, 50f, 10f).sqrMagnitude);
+        Debug.Log(new Vector3(38.3022f, 32.1393f, 10f).sqrMagnitude);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
